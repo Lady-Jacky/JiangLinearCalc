6d18302dd417dd628a5bc4412c011b4443418dfb
1E10 laptop <sample@bths.edu>
2022-10-17 11:02:58 -0400

    Distance formula

--- src/LinearCalculator.java
+++ src/LinearCalculator.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class LinearCalculator {
+public class LinearCalculator {
+    private int x1;
+    private int y1;
+    private int x2;
+    private int y2;
+
+    public LinearCalculator(int x1, int y1, int x2, int y2) {
+        this.x1 = x1;
+        this.x2 = x2;
+        this.y1 = y1;
+        this.y2 = y2;
+    }
+
+    public double distance() {
+        double far = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        return  far;
+    }
 }
--- src/LinearRunner.java
+++ src/LinearRunner.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class LinearRunner {
+import java.util.Scanner;
+public class LinearRunner {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter coords: ");
+        String cord1 = scan.nextLine();
+        System.out.print("Enter coords: ");
+        String cord2 = scan.nextLine();
+
+        int indexOfX1 = cord1.indexOf(",");
+        int indexOfX2 = cord1.indexOf(",");
+        int indexOfY1 = cord2.indexOf(")");
+        int indexOfY2 = cord2.indexOf(")");
+
+        int x1 = Integer.parseInt(cord1.substring(1,indexOfX1));
+        int y1 = Integer.parseInt(cord1.substring(indexOfX1 + 2,indexOfY1));
+        int x2 = Integer.parseInt(cord2.substring(1,indexOfX2));
+        int y2 = Integer.parseInt(cord2.substring(indexOfX2 + 2,indexOfY2));
+
+
+    }
 }
